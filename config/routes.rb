ef91effdc951f677b3de02081ef864eb414f6f5e
Gary Riger <grigerny@aol.com>
2012-05-08 22:02:30 -0400

    added root routes etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 SampleApp::Application.routes.draw do
+ root :to => 'static_pages#home'
+ match '/help', :to => 'static_pages#help'
+ match '/about', :to => 'static_pages#about'
+ match '/contact', :to => 'static_pages#contact'
  
-  get "users/new"
-
- get "static_pages/home" 
- get "static_pages/help"
- get "static_pages/about"
- get "static_pages/contact"
+ get "users/new"
+ match '/signup',  :to => 'users#new'
 end
 
 
@@ -58,7 +58,6 @@ end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
