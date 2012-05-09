6c06146dab585e49638a8d656dfd68240aa916c5
Gary Riger <grigerny@aol.com>
2012-05-08 21:41:16 -0400

    added Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
  
+  get "users/new"
+
  get "static_pages/home" 
  get "static_pages/help"
  get "static_pages/about"
@@ -56,7 +58,7 @@ end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'static_pages#home'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
